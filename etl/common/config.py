eefa4341c875d3eab219f1d84a56b1611e5e83ba
Nicolás <n.buenaventura@udea.edu.co>
2025-12-21 14:32:21 -0500

    Se modifica el archivo de acuerdo a la API seleccionada pidiendo 190 registros

--- etl/common/config.py
+++ etl/common/config.py
@@ -11,12 +11,12 @@ def load_config() -> Dict[str, Any]:
 
     cfg: Dict[str, Any] = {
         "MONGO_URI": os.getenv("MONGO_URI", "mongodb://localhost:27017"),
-        "MONGO_DB": os.getenv("MONGO_DB", "etl_demo"),
-        "MONGO_RAW_COLLECTION": os.getenv("MONGO_RAW_COLLECTION", "pokemon_raw"),
-        "POKEAPI_BASE_URL": os.getenv("POKEAPI_BASE_URL", "https://pokeapi.co/api/v2"),
-        "POKEAPI_LIMIT": int(os.getenv("POKEAPI_LIMIT", "25")),
-        # MySQL connection URL, e.g. mysql+pymysql://user:pass@host:3306/pokemon
-        "DATABASE_URL": os.getenv("DATABASE_URL", "mysql+pymysql://root:root@localhost:3306/pokemon"),
+        "MONGO_DB": os.getenv("MONGO_DB", "taller4_db"), #Base de datos a utilizar 
+        "MONGO_RAW_COLLECTION": os.getenv("MONGO_RAW_COLLECTION", "raw_data"), #Datos crudos
+        "JUEGOS_BASE_URL": os.getenv("JUEGOS_BASE_URL", "https://www.freetogame.com/api/games"),
+        "JUEGOS_LIMIT": int(os.getenv("JUEGOS_LIMIT", "190")), #Límite de >=100 datos según el taller
+        # MySQL connection URL, e.g. mysql+pymysql://user:pass@host:3306/juegos
+        "DATABASE_URL": os.getenv("DATABASE_URL"),
     }
 
     # Ensure data directories exist
