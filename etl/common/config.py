1f7f7a2755a5713a4048f7ae964edda8482a3113
Nicolás <n.buenaventura@udea.edu.co>
2025-12-21 14:45:13 -0500

    Se agregan un par de comentarios para mejor entendimiento del código

--- etl/common/config.py
+++ etl/common/config.py
@@ -10,8 +10,10 @@ def load_config() -> Dict[str, Any]:
     load_dotenv()
 
     cfg: Dict[str, Any] = {
-        "MONGO_URI": os.getenv("MONGO_URI", "mongodb://localhost:27017"),
+        "MONGO_URI": os.getenv("MONGO_URI", "mongodb://localhost:27017"), #Dirección para conectarse a MongoDB
+        #Se le pide que se conecte a MongoDB usando la URI definida en el sistema; si no hay ninguna, usar MongoDB local
         "MONGO_DB": os.getenv("MONGO_DB", "taller4_db"), #Base de datos a utilizar 
+        #El nombre de la base de datos de MongoDB será el que esté definido en el sistema; si no hay ninguno definido, usar 'taller4_db'.
         "MONGO_RAW_COLLECTION": os.getenv("MONGO_RAW_COLLECTION", "raw_data"), #Datos crudos
         "JUEGOS_BASE_URL": os.getenv("JUEGOS_BASE_URL", "https://www.freetogame.com/api/games"),
         "JUEGOS_LIMIT": int(os.getenv("JUEGOS_LIMIT", "190")), #Límite de >=100 datos según el taller
